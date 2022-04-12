8beb645a13758904fed98e57dc6fc780d512371f
Diego García <diego350z1@gmail.com>
2022-04-12 09:03:38 -0600

    Cambio de ruta / redirige hacia /login

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/users', function () {
-    return view('users.index');
-})->name('ejemplo');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
