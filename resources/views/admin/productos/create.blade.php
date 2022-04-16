2dd5cdbe3668779e333647dee478f9544e006073
Diego García <diego350z1@gmail.com>
2022-04-15 21:32:11 -0600

    Creación de vista para Editar productos

--- resources/views/admin/productos/create.blade.php
+++ resources/views/admin/productos/create.blade.php
@@ -3,10 +3,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
+                <h2 class="text-xl mx-4">Creación de Producoto</h2>
+
                 <div class="flex justify-center">
                     <div class="w-1/2">
 
-                        <form class="flex flex-col"  method="POST" action=" route{{'admin.productos.store'}} ">
+                        <form class="flex flex-col my-8"  method="POST" action=" route{{'admin.productos.store'}} ">
                             @csrf
 
                             <div>
@@ -30,21 +32,26 @@
                             </div>
 
                             <div>
-                                <x-jet-label for="sku" value="{{ __('SKU') }}" />
-                                <x-jet-input id="sku" class="block mt-1 w-full" type="text-area" name="sku" :value="old('sku')" required autofocus autocomplete="sku" />
+                                <x-jet-label for="description" value="{{ __('Descripción') }}" />
+                                <x-jet-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus autocomplete="description" />
                             </div>
+
+                            <div>
+                                <x-jet-label for="costo" value="{{ __('Costo') }}" />
+                                <x-jet-input id="costo" class="block mt-1 w-full" type="number" name="costo" :value="old('costo')" required autofocus autocomplete="costo" />
+                            </div>
+
+                            {{-- Existencias por default se crean en CERO --}}
 
                             <div class="flex items-center justify-end mt-4">
                                 <x-jet-button class="ml-4">
-                                    {{ __('Reguistrar Usuario') }}
+                                    {{ __('Crear Producto') }}
                                 </x-jet-button>
                             </div>
 
                         </form>
 
                     </div>
-
-
 
                 </div>
 
