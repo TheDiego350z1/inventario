9413eb035a07d568baa24eabaf9715c097ec2d9c
Diego García <diego350z1@gmail.com>
2022-05-02 22:41:07 -0600

    Creación de componente para ver proveedores

---
+++ app/Http/Livewire/Proveedores/VerProveedores.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Proveedores;
+
+use Livewire\Component;
+
+class VerProveedores extends Component
+{
+    public function render()
+    {
+        return view('livewire.proveedores.ver-proveedores');
+    }
+}
