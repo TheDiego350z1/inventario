89e7586a01af27f9689e9c53e94c7bcd77d93d69
Diego García <diego350z1@gmail.com>
2022-06-03 20:32:12 -0600

    Actualización de vista clientes y modal crear-clientes

--- resources/views/livewire/clientes/crear-cliente.blade.php
+++ resources/views/livewire/clientes/crear-cliente.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <button class="button-blue" wire:click="$set('open', true)">
+    <button class="btn" wire:click="$set('open', true)">
         Crear cliente
     </button>
 
@@ -10,39 +10,97 @@
         </x-slot>
 
         <x-slot name="content">
-            <div>
-                <x-jet-label for="nombre" value="{{ __('Nombre') }}" />
-                <x-jet-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required autofocus autocomplete="nombre" wire:model="nombre" />
+
+            <div class="flex">
+                {{-- Nombre del cliente --}}
+                <div class="mb-4 w-full mx-2">
+                    <x-jet-label for="nombre" value="{{ __('Nombre') }}" />
+                    <x-jet-input
+                        id="nombre"
+                        class="block mt-1 w-full"
+                        type="text"
+                        name="nombre"
+                        :value="old('nombre')"
+                        required
+                        autofocus
+                        autocomplete="nombre"
+                        wire:model="nombre"
+                    />
+                </div>
+
+                <div class="mb-4 w-full mx-2">
+                    {{-- NIT del cliente --}}
+                    <x-jet-label for="nit" value="{{ __('NIT') }}" />
+                    <x-jet-input
+                        id="nit"
+                        class="block mt-1 w-full"
+                        type="text"
+                        name="nit"
+                        :value="old('nit')"
+                        required
+                        autofocus
+                        autocomplete="nit"
+                        wire:model="nit"
+                    />
+                </div>
+            </div>
+
+            <div class="flex">
+                {{-- RUC del cliente --}}
+                <div class="mb-4 w-full mx-2">
+                    <x-jet-label for="ruc" value="{{ __('RUC') }}" />
+                    <x-jet-input
+                        id="ruc"
+                        class="block mt-1 w-full"
+                        type="text"
+                        name="ruc"
+                        :value="old('ruc')"
+                        required
+                        autofocus
+                        autocomplete="ruc"
+                        wire:model="ruc"
+                    />
+                </div>
+
+                <div class="mb-4 w-full mx-2">
+                    {{-- DUI del cliente --}}
+                    <x-jet-label for="dui" value="{{ __('DUI') }}" />
+                    <x-jet-input
+                        id="dui"
+                        class="block mt-1 w-full"
+                        type="text"
+                        name="dui"
+                        :value="old('dui')"
+                        required
+                        autofocus
+                        autocomplete="dui"
+                        wire:model="dui"
+                    />
+                </div>
             </div>
 
             <div>
-                <x-jet-label for="nit" value="{{ __('NIT') }}" />
-                <x-jet-input id="nit" class="block mt-1 w-full" type="text" name="nit" :value="old('nit')" required autofocus autocomplete="nit" wire:model="nit" />
-            </div>
-
-            <div>
-                <x-jet-label for="ruc" value="{{ __('RUC') }}" />
-                <x-jet-input id="ruc" class="block mt-1 w-full" type="text" name="ruc" :value="old('ruc')" required autofocus autocomplete="ruc" wire:model="ruc" />
-            </div>
-
-            <div>
-                <x-jet-label for="dui" value="{{ __('DUI') }}" />
-                <x-jet-input id="dui" class="block mt-1 w-full" type="text" name="dui" :value="old('dui')" required autofocus autocomplete="dui" wire:model="dui"  />
-            </div>
-
-            <div>
+                {{-- Descripción del cliente --}}
                 <x-jet-label for="description" value="{{ __('Descripción') }}" />
-                <textarea  class="textarea block mt-1 w-full" rows="5" wire:model="descripcion"></textarea>
+                <textarea
+                    class="textarea textarea-bordered block mt-1 w-full"
+                    rows="5"
+                    wire:model="descripcion"
+                >
+                </textarea>
             </div>
 
         </x-slot>
 
         <x-slot name="footer">
-            <button class="button-white">
+            <button
+                class="btn btn-sm btn-primary mx-8"
+                wire:click="abort"
+            >
                 cancelar
             </button>
 
-            <button class="button-blue" wire:click="crear">
+            <button class="btn btn-sm" wire:click="crear">
                 crear cliente
             </button>
 
